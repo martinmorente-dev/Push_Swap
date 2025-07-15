3e17b57cb0c29a230d66ba51f3c4e192d8c4e8d1
Martin Morente Vargas <martin.dev45@gmail.com>
2025-07-15 14:16:32 +0200

    CREATE
    Creation of the repo and the first try of organisation (I need to keep trying or ask for some help in 42)

---
+++ validations.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validations.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmorente <mmorente@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 14:07:01 by mmorente          #+#    #+#             */
+/*   Updated: 2025/07/15 14:12:05 by mmorente         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*  The error need to be displayed like this Error\n
+ * Error if it encounter a character 
+ * Overpass the limits of the int 
+ */ 
+
